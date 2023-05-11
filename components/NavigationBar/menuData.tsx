5b2724bde53e644ad1e5b0d378fb75170e69fa3b
Natanael Pacheco <natepacheco716@gmail.com>
2023-05-11 05:00:35 -0500

    First Commit

---
+++ components/NavigationBar/menuData.tsx
@@ -0,0 +1,29 @@
+import { Menu } from "@/types/menu";
+
+const menuData: Menu[] = [
+  {
+    id: 1,
+    title: "What We Do",
+    path: "/",
+    newTab: false,
+  },
+  {
+    id: 2,
+    title: "Ministries",
+    path: "/about",
+    newTab: false,
+  },
+  {
+    id: 3,
+    title: "Events",
+    path: "/blog",
+    newTab: false,
+  },
+  {
+    id: 4,
+    title: "Contact",
+    path: "/contact",
+    newTab: false,
+  },
+];
+export default menuData;
