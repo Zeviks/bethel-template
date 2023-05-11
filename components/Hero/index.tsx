36163206f5db3ddf28ee046245598dbc4fd1ee27
Natanael Pacheco <natepacheco716@gmail.com>
2023-05-11 06:07:11 -0500

    Adjusted hero

--- components/Hero/index.tsx
+++ components/Hero/index.tsx
@@ -11,30 +11,35 @@ const Hero = () => {
           <div className="-mx-4 flex flex-wrap">
             <div className="w-full px-4">
               <div
-                className="wow fadeInUp mx-auto max-w-[800px] text-center"
+                className="wow fadeInUp mx-auto max-w-[800px] text-center xl:max-w-[900px]"
                 data-wow-delay=".2s"
               >
-                <h1 className="mb-5 text-3xl font-bold leading-tight text-black dark:text-white sm:text-4xl sm:leading-tight md:text-5xl md:leading-tight">
-                  Serving God with Love
+                <h1 className="m mb-5 font-['Bellefair'] text-5xl leading-tight text-white dark:text-white sm:text-xl sm:leading-tight md:mt-2  md:text-8xl md:leading-tight  xl:text-9xl">
+                  SERVING GOD
+                  <br />
+                  <span>
+                    WITH{" "}
+                    <span className="-py-5 rounded-md bg-primary text-dark">
+                      LOVE
+                    </span>
+                  </span>
                 </h1>
-                <p className="mb-12 text-base font-medium !leading-relaxed text-body-color dark:text-white dark:opacity-90 sm:text-lg md:text-xl">
-                  Startup is free Next.js template for startups and SaaS
-                  business websites comes with all the essential pages,
-                  components, and sections you need to launch a complete
-                  business website, built-with Next 13.x and Tailwind CSS.
+                <p className="mb-12 text-center text-2xl font-light text-white sm:text-lg md:text-xl">
+                  Experience God’s presence with us at Bethel in Pharr,
+                  <br /> on Sundays & Wednesdays.
                 </p>
-                <div className="flex flex-col items-center justify-center space-y-4 sm:flex-row sm:space-x-4 sm:space-y-0">
+                <div className="px-auto flex items-center justify-center space-y-4 sm:flex-row sm:space-x-4 sm:space-y-0">
                   <Link
                     href="https://nextjstemplates.com/templates/startup"
-                    className="rounded-md bg-primary py-4 px-8 text-base font-semibold text-white duration-300 ease-in-out hover:bg-primary/80"
+                    className="rounded-md bg-primary py-3 px-6 text-base font-semibold text-dark duration-300 ease-in-out hover:bg-primary/80"
                   >
-                    Download Now
+                    Join in Person
                   </Link>
                   <Link
                     href="https://github.com/NextJSTemplates/startup-nextjs"
-                    className="rounded-md bg-black/20 py-4 px-8 text-base font-semibold text-black duration-300 ease-in-out hover:bg-black/30 dark:bg-white/20 dark:text-white dark:hover:bg-white/30"
+                    className="rounded-md bg-black/20 py-3 px-6 text-base font-semibold text-lightPrimary duration-300 ease-in-out hover:bg-black/30"
                   >
-                    Star on GitHub
+                    Watch Online
                   </Link>
                 </div>
               </div>
