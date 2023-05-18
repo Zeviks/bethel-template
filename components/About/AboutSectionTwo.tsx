e1972b087b34cc640322a3fdc2b8211daa970a7e
Natanael Pacheco <natepacheco716@gmail.com>
2023-05-17 22:59:12 -0500

    Text commit

--- components/About/AboutSectionTwo.tsx
+++ components/About/AboutSectionTwo.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 const AboutSectionTwo = () => {
   return (
     <section className="py-16 md:py-20 lg:py-28">
-      <div className="h-[100vh] w-full bg-dark"></div>
+      <div className="h-[90vh] w-full bg-dark"></div>
     </section>
   );
 };
