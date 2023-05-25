c6223de34ec0d5b6c3910fc4d3c44b58f4a8ae42
Natanael Pacheco <natepacheco716@gmail.com>
2023-05-25 01:55:26 -0500

    Adjust layout for leaders section

--- components/About/AboutSectionOne.tsx
+++ components/About/AboutSectionOne.tsx
@@ -4,12 +4,14 @@ const AboutSectionOne = () => {
   return (
     <section id="about" className="my-20">
       <SectionHeading heading="Our Mission" />
-      <div className="container mt-10 flex w-full items-center justify-between">
-        <h2 className=" text-4xl font-semibold text-dark">
-          A promise to teach the truth <br /> and nothing but the truth.
+      <div className="container mx-auto mt-10 flex flex-col items-center justify-between lg:flex-row">
+        <h2 className="mb-6 text-center text-4xl font-semibold text-dark lg:mb-0 lg:text-left lg:text-5xl">
+          A promise to teach the truth <br className="lg:hidden" /> and nothing
+          but the truth.
         </h2>
-        <p className="text-xl font-light">
-          Every believer was created to belong to a community. <br />
+        <p className="text-center text-xl font-light lg:text-left lg:text-2xl">
+          Every believer was created to belong to a community.{" "}
+          <br className="lg:hidden" />
           It was never God’s heart for us to do this life alone.
         </p>
       </div>
--- components/Leaders/index.tsx
+++ components/Leaders/index.tsx
@@ -1,5 +1,3 @@
-"use client";
-
 import SectionHeading from "../SectionHeading";
 import LeaderCard from "./LeaderCard";
 
@@ -8,18 +6,21 @@ export default function Leaders() {
     <section className="py-10">
       <SectionHeading heading="Our Leaders" />
 
-      <div className="container my-20 flex w-full flex-col items-center justify-between">
-        <h2 className="w-[30ch] text-center text-5xl font-semibold text-dark">
+      <div className="container my-20 flex flex-col items-center justify-between">
+        <h2 className="w-full text-center text-4xl font-semibold text-dark md:w-[30ch] md:text-5xl">
           Meet our leaders
         </h2>
-        <p className="w-[50ch] pt-3 text-center text-xl font-light leading-tight">
+        <p className="w-full pt-3 text-center text-xl font-light leading-tight md:w-[50ch]">
           Every believer was created to belong to a community. It was never
           God’s heart for us to do this life alone.
         </p>
 
-        <div className="container pt-20">
-          <div className="grid grid-cols-1 gap-x-8 gap-y-10 md:grid-cols-2 md:gap-x-6 lg:gap-x-8 xl:grid-cols-4">
+        <div className="pt-20">
+          <div className="grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-5">
             <div className="w-full">
+              <LeaderCard />
+            </div>
+            <div className=" w-full">
               <LeaderCard />
             </div>
             <div className="w-full">
