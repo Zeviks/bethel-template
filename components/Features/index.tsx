ac4f019e709dda09d5d4cab2ab12c66a14addbc0
Natanael Pacheco <natepacheco716@gmail.com>
2023-05-25 14:42:08 -0500

    Updated features section

--- components/Features/index.tsx
+++ components/Features/index.tsx
@@ -15,37 +15,38 @@ const Features = () => {
         <div className="container">
           <SectionTitle
             title="We are a congregation firmly grounded in the love of God, devoted to serving him under any circumstance."
-            paragraph="There are many variations of passages of Lorem Ipsum available but the majority have suffered alteration in some form."
+            paragraph=""
           />
+
           {/* Grid */}
 
-          <div className="container mt-10">
-            <div className="-mx-4 flex flex-wrap items-center">
-              <div className="w-full px-4 lg:w-1/2">
-                <div
-                  className="wow fadeInUp relative mx-auto mb-12 aspect-[25/24] max-w-[550px] text-center lg:m-0"
-                  data-wow-delay=".15s"
+          <div className="flex flex-col gap-5 pt-10 lg:flex-row">
+            <div className="flex h-auto items-end justify-center rounded bg-dark px-12 lg:h-[500px] lg:w-[700px]">
+              <div className="container mb-12 flex h-[80px] w-[400px] flex-col items-start justify-center rounded bg-primary">
+                <h2 className="text-center text-lg font-light text-white">
+                  We are located at:
+                </h2>
+                <Link
+                  href="/"
+                  className="text-md mt-2 font-semibold text-white"
                 >
-                  <Image
-                    src="/images/visit.jpg"
-                    alt="about image"
-                    fill
-                    className="rounded object-cover"
-                  />
-                </div>
+                  400 S. I Rd, S Veterans Blvd, Pharr, TX 78577
+                </Link>
               </div>
-              <div className="w-full px-4 lg:w-1/2">
-                <div
-                  className="wow fadeInUp relative mx-auto mb-12 aspect-[25/24] max-w-[550px] text-center lg:m-0"
-                  data-wow-delay=".15s"
-                >
-                  <Image
-                    src="/images/visit.jpg"
-                    alt="about image"
-                    fill
-                    className="rounded object-cover"
-                  />
-                </div>
+            </div>
+            <div className="flex h-auto w-full items-end justify-center rounded bg-dark px-12 lg:h-[500px]">
+              <div className="container mb-12 flex items-center justify-center rounded bg-primary py-5">
+                <h2 className="text-center text-xl font-medium text-white">
+                  Our Service Times
+                </h2>
+                <div className="mt-2 mb-4 h-full w-1 rounded bg-white" />
+                <p className="text-center text-lg font-light text-white">
+                  <span className="font-medium">Wednesdays</span> at{" "}
+                  <span className="font-medium">7pm</span> <br />
+                  <span className="font-medium">Sundays</span> at{" "}
+                  <span className="font-medium">10am</span> &{" "}
+                  <span className="font-medium">5pm</span>
+                </p>
               </div>
             </div>
           </div>
@@ -56,3 +57,33 @@ const Features = () => {
 };
 
 export default Features;
+
+/* 
+<div className="flex justify-between gap-5 pt-10">
+            <div className="flex h-[500px] w-[700px] items-end justify-center rounded bg-dark px-12">
+              <div className="container mb-12 flex h-[80px] w-[400px] flex-col justify-center rounded bg-primary">
+                <h2 className="text-lg font-light text-white">
+                  We are located at:
+                </h2>
+                <Link href="/" className="text-md font-semibold text-white">
+                  400 S. I Rd, S Veterans Blvd, Pharr, TX 78577
+                </Link>
+              </div>
+            </div>
+            <div className="flex h-[500px] w-full items-end justify-center rounded bg-dark px-12">
+              <div className="container mb-12 flex h-[80px] w-full items-center justify-evenly rounded bg-primary py-5">
+                <h2 className="text-xl font-medium text-white">
+                  Our Service Times
+                </h2>
+                <div className="h-full w-1 rounded  border-r-2" />
+                <p className="text-lg font-light text-white">
+                  <span className="font-medium">Wednesdays</span> at{" "}
+                  <span className="font-medium">7pm</span> <br />
+                  <span className="font-medium">Sundays</span> at{" "}
+                  <span className="font-medium">10am</span> &{" "}
+                  <span className="font-medium">5pm</span>
+                </p>
+              </div>
+            </div>
+          </div>
+*/
