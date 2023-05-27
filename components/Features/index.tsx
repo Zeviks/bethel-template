4128fccf2cfd5b4d20ba4051fdc7146fbc314cb5
Natanael Pacheco <natepacheco716@gmail.com>
2023-05-26 23:31:54 -0500

    Adjusted some styles

--- components/Features/index.tsx
+++ components/Features/index.tsx
@@ -17,9 +17,7 @@ const Features = () => {
             title="We are a congregation firmly grounded in the love of God, devoted to serving him under any circumstance."
             paragraph=""
           />
-
           {/* Grid */}
-
           <div className="flex flex-col gap-5 pt-10 lg:flex-row">
             <div className="flex h-auto items-end justify-center rounded bg-dark px-12 lg:h-[500px] lg:w-[700px]">
               <div className="container mb-12 flex h-[80px] w-[400px] flex-col items-start justify-center rounded bg-primary">
@@ -40,7 +38,7 @@ const Features = () => {
                   Our Service Times
                 </h2>
                 <div className="mt-2 mb-4 h-full w-1 rounded bg-white" />
-                <p className="text-center text-lg font-light text-white">
+                <p className="text-lg font-light text-white">
                   <span className="font-medium">Wednesdays</span> at{" "}
                   <span className="font-medium">7pm</span> <br />
                   <span className="font-medium">Sundays</span> at{" "}
