e085edf8d6ab3f6ab59271ce1434c869013d67b0
Natanael Pacheco <natepacheco716@gmail.com>
2023-05-27 22:21:26 -0500

    updated font weight

--- components/Features/index.tsx
+++ components/Features/index.tsx
@@ -19,34 +19,36 @@ const Features = () => {
           />
           {/* Grid */}
           <div className="flex flex-col gap-5 pt-10 lg:flex-row">
-            <div className="flex h-auto items-end justify-center rounded bg-dark px-12 lg:h-[500px] lg:w-[700px]">
-              <div className="container mb-12 flex h-[80px] w-[400px] flex-col items-start justify-center rounded bg-primary">
-                <h2 className="text-center text-lg font-light text-white">
+            <div className="flex h-auto items-end justify-center rounded bg-bethelHero bg-cover lg:h-[500px] lg:w-[400px] lg:px-8 xl:w-[700px] xl:px-12">
+              <div className="container mb-12 flex h-[80px] w-[400px] flex-col items-start justify-center rounded bg-dark">
+                <h2 className="lg:text-md text-center font-light text-white xl:text-lg">
                   We are located at:
                 </h2>
                 <Link
                   href="/"
-                  className="text-md mt-2 font-semibold text-white"
+                  className="xl:text-md mt-2 font-semibold text-white lg:text-sm"
                 >
                   400 S. I Rd, S Veterans Blvd, Pharr, TX 78577
                 </Link>
               </div>
             </div>
-            <div className="flex h-auto w-full items-end justify-center rounded bg-dark px-12 lg:h-[500px]">
-              <div className="container mb-12 flex items-center justify-center rounded bg-primary py-5">
-                <h2 className="text-center text-xl font-medium text-white">
+
+            {/* Right Square */}
+            <div className="flex h-auto w-full items-end justify-center rounded bg-bethelHero bg-cover px-12 lg:h-[500px]">
+              <div className="container mb-12 flex h-[80px] items-center justify-evenly rounded bg-dark py-5">
+                <h2 className="lg:text-md  text-center font-medium text-white xl:text-lg">
                   Our Service Times
                 </h2>
-                <div className="mt-2 mb-4 h-full w-1 rounded bg-white" />
-                <p className="text-lg font-light text-white">
-                  <span className="font-medium">Wednesdays</span> at{" "}
-                  <span className="font-medium">7pm</span> <br />
-                  <span className="font-medium">Sundays</span> at{" "}
-                  <span className="font-medium">10am</span> &{" "}
-                  <span className="font-medium">5pm</span>
+                <p className="font-light text-white lg:text-sm xl:text-lg">
+                  <span className="font-semibold">Wednesdays</span> at{" "}
+                  <span className="font-semibold">7pm</span> <br />
+                  <span className="font-semibold">Sundays</span> at{" "}
+                  <span className="font-semibold">10am</span> &{" "}
+                  <span className="font-semibold">5pm</span>
                 </p>
               </div>
             </div>
+            {/* Bottom.... */}
           </div>
         </div>
       </section>
