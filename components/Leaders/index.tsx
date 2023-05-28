dcd82659efb895fd3cac6abac6dc703d2d39fdeb
Natanael Pacheco <natepacheco716@gmail.com>
2023-05-28 02:03:31 -0500

    Implemented image slider.

--- components/Leaders/index.tsx
+++ components/Leaders/index.tsx
@@ -1,7 +1,81 @@
+"use client";
 import SectionHeading from "../SectionHeading";
-import LeaderCard from "./LeaderCard";
+import Image from "next/image";
+
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 export default function Leaders() {
+  const developers = [
+    {
+      name: "John Doe",
+      jobTitle: "Frontend Developer",
+      image:
+        "https://images.unsplash.com/photo-1682407186023-12c70a4a35e0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2832&q=80",
+    },
+    {
+      name: "Jane Smith",
+      jobTitle: "Backend Developer",
+      image:
+        "https://images.unsplash.com/photo-1682407186023-12c70a4a35e0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2832&q=80",
+    },
+    {
+      name: "David Johnson",
+      jobTitle: "Full Stack Developer",
+      image:
+        "https://images.unsplash.com/photo-1682407186023-12c70a4a35e0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2832&q=80",
+    },
+    {
+      name: "David Johnson",
+      jobTitle: "Full Stack Developer",
+      image:
+        "https://images.unsplash.com/photo-1682407186023-12c70a4a35e0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2832&q=80",
+    },
+    {
+      name: "David Johnson",
+      jobTitle: "Full Stack Developer",
+      image:
+        "https://images.unsplash.com/photo-1682407186023-12c70a4a35e0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2832&q=80",
+    },
+    // Add more developer objects as needed
+  ];
+
+  let settings = {
+    className: "",
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 5,
+    slidesToScroll: 3,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: false,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          initialSlide: 2,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  };
+
   return (
     <section className="py-10">
       <SectionHeading heading="Our Leaders" />
@@ -15,26 +89,47 @@ export default function Leaders() {
           God’s heart for us to do this life alone.
         </p>
 
-        <div className="pt-20">
-          <div className="grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-5">
-            <div className="w-full">
-              <LeaderCard />
-            </div>
-            <div className=" w-full">
-              <LeaderCard />
-            </div>
-            <div className="w-full">
-              <LeaderCard />
-            </div>
-            <div className="w-full">
-              <LeaderCard />
-            </div>
-            <div className="w-full">
-              <LeaderCard />
-            </div>
-          </div>
+        <div className="container pt-20">
+          <Slider {...settings}>
+            {developers.map((developer, index) => (
+              <div key={index} className="h-[300px] w-[200px]  px-2 ">
+                <img
+                  src={developer.image}
+                  alt={developer.name}
+                  className="h-full w-full object-cover shadow-sm shadow-blue-gray-900/50"
+                />
+                <div className="pt-3">
+                  <h3 className="font-medium">{developer.name}</h3>
+                  <p className="font-light text-primary">
+                    {developer.jobTitle}
+                  </p>
+                </div>
+              </div>
+            ))}
+          </Slider>
         </div>
       </div>
     </section>
   );
 }
+
+/* 
+          <Slider {...settings}>
+            <div className=" h-[300px] w-[200px] bg-dark">
+              <h3 className="text-white">1</h3>
+            </div>
+            <div className=" h-[300px] w-[200px] bg-dark">
+              <h3 className="text-white">2</h3>
+            </div>
+            <div className=" h-[300px] w-[200px] bg-dark">
+              <h3 className="text-white">3</h3>
+            </div>
+            <div className=" h-[300px] w-[200px] bg-dark">
+              <h3 className="text-white">4</h3>
+            </div>
+            <div className=" h-[300px] w-[200px] bg-dark">
+              <h3 className="text-white">5</h3>
+            </div>
+          </Slider>
+
+*/
