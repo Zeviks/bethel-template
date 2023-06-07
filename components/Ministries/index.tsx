ca889e4d53acd66fa4defb756a483d7ae1eddd6b
Natanael Pacheco <natepacheco716@gmail.com>
2023-06-06 23:07:36 -0500

    Adjusted ministries section

--- components/Ministries/index.tsx
+++ components/Ministries/index.tsx
@@ -1,10 +1,46 @@
-"use client";
+import { useState } from "react";
 import Image from "next/image";
 import SectionHeading from "../SectionHeading";
-import { Button } from "@material-tailwind/react";
+
 import MinistryCard from "./MinistryCard";
+import { createPortal } from "react-dom";
+import { flushSync } from "react-dom/cjs/react-dom-server.browser.development"; // Update the import path
 
 const Ministries = () => {
+  const ministriesData = [
+    {
+      title: "Women Ministries",
+      description: "Meets 7pm to 5pm @ Fellowship hall",
+      imageURL: "/images/hero/hero.png",
+    },
+    {
+      title: "Men Ministries",
+      description: "Meets 7pm to 5pm @ Fellowship hall",
+      imageURL: "/images/hero/hero.png",
+    },
+    {
+      title: "Kids Ministries",
+      description: "Meets 7pm to 5pm @ Fellowship hall",
+      imageURL: "/images/hero/hero.png",
+    },
+    {
+      title: "Women Ministries",
+      description: "Meets 7pm to 5pm @ Fellowship hall",
+      imageURL: "/images/hero/hero.png",
+    },
+    {
+      title: "Women Ministries",
+      description: "Meets 7pm to 5pm @ Fellowship hall",
+      imageURL: "/images/hero/hero.png",
+    },
+    {
+      title: "Women Ministries",
+      description: "Meets 7pm to 5pm @ Fellowship hall",
+      imageURL: "/images/hero/hero.png",
+    },
+    // Add more ministry objects as needed
+  ];
+
   return (
     <section className="yearly-gradient py-10">
       <SectionHeading heading="Our Ministries" />
@@ -23,26 +59,25 @@ const Ministries = () => {
 
       <div className="container">
         <div className="grid grid-cols-2 grid-rows-[_repeat(3,_250px)] gap-4 rounded-lg md:grid-cols-3">
-          {/* Top Row */}
-          <div className="row-span-1">
-            <MinistryCard />
-          </div>
-          <div className="row-span-2 ">
-            <MinistryCard />
-          </div>
-          <div className="row-span-1 ">
-            <MinistryCard />
-          </div>
-          {/* Bottom Row */}
-          <div className="row-span-2 ">
-            <MinistryCard />
-          </div>
-          <div className="row-span-2 ">
-            <MinistryCard />
-          </div>
-          <div className="row-span-1 ">
-            <MinistryCard />
-          </div>
+          {ministriesData.map((ministry, index) => (
+            <div className={`row-span-${index === 0 ? 1 : 2}`} key={index}>
+              <div
+                className="h-[100%] overflow-hidden rounded bg-cover p-4 shadow-lg"
+                style={{ backgroundImage: `url('${ministry.imageURL}')` }}
+              >
+                <div className="flex h-[100%] flex-col justify-between">
+                  <div>
+                    <h5 className="text-neutral-800  mb-2 text-xl font-medium leading-tight text-white">
+                      {ministry.title}
+                    </h5>
+                    <p className="text-neutral-600  mb-4 text-base font-light  text-white">
+                      {ministry.description}
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          ))}
         </div>
 
         <div className="my-10 flex justify-between">
@@ -51,7 +86,7 @@ const Ministries = () => {
             pellentesque quis mus. pellentesque a mus viverra sit.
           </p>
           <a href="/ministries">
-            <Button variant="outlined">View All</Button>
+            <button>View All</button>
           </a>
         </div>
       </div>
