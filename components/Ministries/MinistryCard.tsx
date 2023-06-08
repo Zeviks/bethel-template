4dfb8019f6550121b3a436d81008b3692cab6cf1
Natanael Pacheco <natepacheco716@gmail.com>
2023-06-08 02:25:48 -0500

    Made layout changes to ministries section.

--- components/Ministries/MinistryCard.tsx
+++ components/Ministries/MinistryCard.tsx
@@ -1,21 +1,26 @@
-"use client";
-import Image from "next/image";
-import { useState } from "react";
+interface MinistryCardProps {
+  title: string;
+  description: string;
+  imageURL: string;
+}
 
-const MinistryCard = () => {
-  const [hover, setHover] = useState(false);
+const MinistryCard: React.FC<MinistryCardProps> = ({
+  title,
+  description,
+  imageURL,
+}) => {
   return (
     <div
       className="h-[100%] overflow-hidden rounded bg-cover p-4 shadow-lg"
-      style={{ backgroundImage: "url('/images/hero/hero.png')" }}
+      style={{ backgroundImage: `url('${imageURL}')` }}
     >
-      <div className="flex h-[100%] flex-col  justify-between">
+      <div className="flex h-[100%] flex-col justify-between">
         <div>
-          <h5 className="text-neutral-800  mb-2 text-xl font-medium leading-tight text-white">
-            Women Ministries
+          <h5 className="text-neutral-800 mb-2 text-xl font-medium leading-tight text-white">
+            {title}
           </h5>
-          <p className="text-neutral-600  mb-4 text-base font-light  text-white">
-            Meets 7pm to 5pm @ Fellowship hall
+          <p className="text-neutral-600 mb-4 text-base font-light text-white">
+            {description}
           </p>
         </div>
       </div>
--- components/Ministries/index.tsx
+++ components/Ministries/index.tsx
@@ -1,57 +1,20 @@
-import { useState } from "react";
-import Image from "next/image";
+"use client";
 import SectionHeading from "../SectionHeading";
-
+import { Button } from "@material-tailwind/react";
 import MinistryCard from "./MinistryCard";
-import { createPortal } from "react-dom";
-import { flushSync } from "react-dom/cjs/react-dom-server.browser.development"; // Update the import path
 
 const Ministries = () => {
-  const ministriesData = [
-    {
-      title: "Women Ministries",
-      description: "Meets 7pm to 5pm @ Fellowship hall",
-      imageURL: "/images/hero/hero.png",
-    },
-    {
-      title: "Men Ministries",
-      description: "Meets 7pm to 5pm @ Fellowship hall",
-      imageURL: "/images/hero/hero.png",
-    },
-    {
-      title: "Kids Ministries",
-      description: "Meets 7pm to 5pm @ Fellowship hall",
-      imageURL: "/images/hero/hero.png",
-    },
-    {
-      title: "Women Ministries",
-      description: "Meets 7pm to 5pm @ Fellowship hall",
-      imageURL: "/images/hero/hero.png",
-    },
-    {
-      title: "Women Ministries",
-      description: "Meets 7pm to 5pm @ Fellowship hall",
-      imageURL: "/images/hero/hero.png",
-    },
-    {
-      title: "Women Ministries",
-      description: "Meets 7pm to 5pm @ Fellowship hall",
-      imageURL: "/images/hero/hero.png",
-    },
-    // Add more ministry objects as needed
-  ];
-
   return (
     <section className="yearly-gradient py-10">
       <SectionHeading heading="Our Ministries" />
-      <div className="container my-20 flex flex-col items-center justify-between lg:flex-row">
-        <h2 className="mb-4 w-full text-4xl font-semibold text-dark md:w-[80%] lg:mb-0 lg:mr-8 lg:w-[30ch]">
+      <div className="container my-20 flex w-full items-center justify-between">
+        <h2 className="w-[30ch] text-4xl font-semibold text-dark">
           God's house always has room for everyone.{" "}
           <span className="border-b-4 border-primary">
             Discover where you fit in best.
           </span>
         </h2>
-        <p className="text-xl font-light lg:text-lg">
+        <p className="text-xl font-light">
           Every believer was created to belong to a community. <br />
           It was never God’s heart for us to do this life alone.
         </p>
@@ -59,25 +22,50 @@ const Ministries = () => {
 
       <div className="container">
         <div className="grid grid-cols-2 grid-rows-[_repeat(3,_250px)] gap-4 rounded-lg md:grid-cols-3">
-          {ministriesData.map((ministry, index) => (
-            <div className={`row-span-${index === 0 ? 1 : 2}`} key={index}>
-              <div
-                className="h-[100%] overflow-hidden rounded bg-cover p-4 shadow-lg"
-                style={{ backgroundImage: `url('${ministry.imageURL}')` }}
-              >
-                <div className="flex h-[100%] flex-col justify-between">
-                  <div>
-                    <h5 className="text-neutral-800  mb-2 text-xl font-medium leading-tight text-white">
-                      {ministry.title}
-                    </h5>
-                    <p className="text-neutral-600  mb-4 text-base font-light  text-white">
-                      {ministry.description}
-                    </p>
-                  </div>
-                </div>
-              </div>
-            </div>
-          ))}
+          {/* Top Row */}
+          <div className="row-span-1">
+            <MinistryCard
+              title="Women Ministries"
+              description="Meets 7pm to 5pm @ Fellowship hall"
+              imageURL="/images/hero/hero.png"
+            />
+          </div>
+          <div className="row-span-2 ">
+            <MinistryCard
+              title="Kids Ministries"
+              description="Meets 7pm to 5pm @ Fellowship hall"
+              imageURL="/images/hero/hero.png"
+            />
+          </div>
+          <div className="row-span-1 ">
+            <MinistryCard
+              title="Youth Ministries"
+              description="Meets 7pm to 5pm @ Fellowship hall"
+              imageURL="/images/hero/hero.png"
+            />
+          </div>
+          {/* Bottom Row */}
+          <div className="row-span-2 ">
+            <MinistryCard
+              title="Golden Agers"
+              description="Meets 7pm to 5pm @ Fellowship hall"
+              imageURL="/images/hero/hero.png"
+            />
+          </div>
+          <div className="row-span-2 ">
+            <MinistryCard
+              title="Royal Rangers"
+              description="Meets 7pm to 5pm @ Fellowship hall"
+              imageURL="/images/hero/hero.png"
+            />
+          </div>
+          <div className="col-span-2 row-span-1 md:col-span-1 ">
+            <MinistryCard
+              title="Girl's Ministries"
+              description="Meets 7pm to 5pm @ Fellowship hall"
+              imageURL="/images/hero/hero.png"
+            />
+          </div>
         </div>
 
         <div className="my-10 flex justify-between">
@@ -86,7 +74,7 @@ const Ministries = () => {
             pellentesque quis mus. pellentesque a mus viverra sit.
           </p>
           <a href="/ministries">
-            <button>View All</button>
+            <Button variant="outlined">View All</Button>
           </a>
         </div>
       </div>
