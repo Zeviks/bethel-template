6dd6bdba99e3192b12d126a324b3b23ee7c50b67
Natanael Pacheco <natepacheco716@gmail.com>
2023-06-26 07:52:01 -0500

    Added validation to the schemas.

--- sanity/schemas/about.ts
+++ sanity/schemas/about.ts
@@ -1,4 +1,5 @@
 import { defineField, defineType } from "sanity";
+import { getExtension, getImageDimensions } from "@sanity/asset-utils";
 
 export default defineType({
   name: "about",
@@ -9,6 +10,26 @@ export default defineType({
       name: "leftImage",
       title: "Left image",
       type: "image",
+      validation: (rule) =>
+        rule.custom((value) => {
+          if (!value) {
+            return "Image field cannot be left empty";
+          }
+
+          const filetype = getExtension(value.asset._ref);
+
+          if (filetype !== "jpg" && filetype !== "png" && filetype !== "webp") {
+            return "Image must be a JPG or PNG";
+          }
+
+          const { width, height } = getImageDimensions(value.asset._ref);
+
+          if (width < 1200 || height < 630) {
+            return "Image must be at least 1200x630 pixels";
+          }
+
+          return true;
+        }),
       options: {
         hotspot: true,
       },
@@ -17,6 +38,26 @@ export default defineType({
       name: "rightImage",
       title: "Right image",
       type: "image",
+      validation: (rule) =>
+        rule.custom((value) => {
+          if (!value) {
+            return "Image field cannot be left empty";
+          }
+
+          const filetype = getExtension(value.asset._ref);
+
+          if (filetype !== "jpg" && filetype !== "png") {
+            return "Image must be a JPG or PNG";
+          }
+
+          const { width, height } = getImageDimensions(value.asset._ref);
+
+          if (width < 1200 || height < 630) {
+            return "Image must be at least 1200x630 pixels";
+          }
+
+          return true;
+        }),
       options: {
         hotspot: true,
       },
--- sanity/schemas/hero.ts
+++ sanity/schemas/hero.ts
@@ -1,5 +1,6 @@
 import { defineField, defineType } from "sanity";
 import defaultImage from "@/utils/defaultImage";
+import { getExtension, getImageDimensions } from "@sanity/asset-utils";
 
 const defaultImageValidation = (rule, context) => {
   if (!context.document.mainImage) {
@@ -17,10 +18,29 @@ export default defineType({
       name: "mainImage",
       title: "Main image",
       type: "image",
+      validation: (rule) =>
+        rule.custom((value) => {
+          if (!value) {
+            return "Image field cannot be left empty";
+          }
+
+          const filetype = getExtension(value.asset._ref);
+
+          if (filetype !== "jpg" && filetype !== "png" && filetype !== "webp") {
+            return "Image must be a JPG or PNG";
+          }
+
+          const { width, height } = getImageDimensions(value.asset._ref);
+
+          if (width < 1200 || height < 630) {
+            return "Image must be at least 1200x630 pixels";
+          }
+
+          return true;
+        }),
       options: {
         hotspot: true,
       },
-      validation: (rule) => rule.custom(defaultImageValidation),
     }),
   ],
 });
